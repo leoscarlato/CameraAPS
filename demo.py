87fe413840fe2ce83d51bf3b35f574f611940268
Leonardo Scarlato <leonardo.scarlato@hotmail.com>
2023-03-19 17:20:09 -0300

    primeira versão sem a função afim

--- demo.py
+++ demo.py
@@ -14,26 +14,27 @@ def main():
     # Acessa a câmera
     camera = cv.VideoCapture(0)
 
-    # Define a largura e altura do vídeo
-    largura = 640
-    altura = 480
+    # Define a largura e altura do vídeo (tamanho pequeno para melhorar a performance)
+    largura = 320
+    altura = 240
     
     # Define o ângulo inicial de rotação
     grau = 0
 
     while True:
+        
         # Captura um quadro da câmera
         captura_ok, quadro = camera.read()
-        
+
+        # Redimensiona o quadro para o tamanho definido
+        quadro = cv.resize(quadro, (largura, altura))
+
         # Calcula as coordenadas do centro da imagem
-        cx, cy = int(largura/2), int(altura/2)
+        cx, cy = int(largura / 2), int(altura / 2)
         
         # Define a matriz de translação que leva o centro da imagem para a origem
         M_translacao = np.array([[1, 0, -cx], [0, 1, -cy], [0, 0, 1]])  # leva para esquerda e para cima
         
-        # Decrementa o ângulo de rotação
-        grau -= 1
-
         # Define a matriz de rotação atual
         rotacao = np.array([[np.cos(np.radians(grau)), -np.sin(np.radians(grau)), 0], 
                         [np.sin(np.radians(grau)), np.cos(np.radians(grau)), 0], 
@@ -44,18 +45,43 @@ def main():
         
         # Aplica as matrizes de transformação para girar a imagem no centro
         M_combinada = M_inv_translacao @ rotacao @ M_translacao
-        quadro_rotacionado = cv.warpAffine(quadro, M_combinada[:2], (largura, altura))  # transformação afim -> 2x3
+
+        # Calcula as coordenadas dos pixels da imagem rotacionada
+        destino = criar_indices(0, altura, 0, largura)  # cria uma matriz com as coordenadas dos pixels da imagem rotacionada
+        destino = np.vstack( (destino, np.ones((1, destino.shape[1]))) )    # acrescenta uma linha com 1s
         
-        # Exibe o quadro com a imagem rotacionada
-        cv.imshow("Camera", quadro_rotacionado)
+        # Calcula as coordenadas dos pixels da imagem origina
         
-        # Verifica se a tecla 'q' foi pressionada para sair do loop
-        if cv.waitKey(1) == ord('q'):
+        A = np.linalg.inv(M_combinada) @ destino    # calcula as coordenadas dos pixels da imagem original
+        A = A.astype(int)  # converte para int32
+
+        # Aplica o filtro para manter apenas os pixels dentro da imagem
+        filtro = (A[0, :] >= 0) & (A[0, :] < altura) & (A[1, :] >= 0) & (A[1, :] < largura)
+        
+        A = A[:, filtro]    # aplica o filtro
+        destino = destino[:, filtro]    # aplica o filtro
+
+        # Cria uma imagem preta
+        quadro_rotacionado = np.zeros((altura, largura, 3), dtype=np.uint8)
+
+        # Copia os pixels da imagem original para a imagem rotacionada
+        quadro_rotacionado[destino[0, :].astype(int), destino[1, :].astype(int)] = quadro[A[0, :], A[1, :]]
+        
+        # Decrementa o ângulo de rotação
+        grau -= 3
+
+        # Mostra a imagem
+        cv.imshow('Imagem', quadro_rotacionado)
+
+        # Aguarda uma tecla ser pressionada
+        if cv.waitKey(1) & 0xFF == ord('q'):
             break
-        
-    # Libera a câmera e fecha todas as janelas
+
+    # Libera a câmera
     camera.release()
+
+    # Fecha todas as janelas
     cv.destroyAllWindows()
-    
-if __name__ == "__main__":
+
+if __name__ == '__main__':
     main()
